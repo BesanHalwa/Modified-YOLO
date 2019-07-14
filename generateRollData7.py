32f510900be742e8fb758f41bed927eb0a4a9c04
ESHAN PANDEY <contacteshanpandey@gmail.com>
2019-07-15 00:44:44 +0530

    Add files via upload

---
+++ generateRollData7.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import glob
+
+files = glob.iglob('VOC2007/JPEGImages/*')
+count = 0
+
+for file in files:
+
+    print(file)
+    count = count + 1
+
+    # load image
+    img = cv2.imread(file)
+    h, w, c = img.shape
+    img = img.reshape((h*w*c))
+
+    # now roll 4 pixels
+    new_img = np.roll(img, 4)
+
+    out_img = abs(new_img - img)
+    out_img = out_img.reshape((h,w,c))
+    cv2.imwrite(file, out_img)
+
+    print('Image Count: ', count)
